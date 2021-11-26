28da5fe9490b9e1f0fe514671fcc5ff6f64feff9
Joseph Redmond <jdredmond2000@gmail.com>
2021-11-26 14:39:00 -0500

    Back to the good ol' days

--- src/main/java/mrnerdy42/keywizard/KeyWizard.java
+++ src/main/java/mrnerdy42/keywizard/KeyWizard.java
@@ -5,17 +5,28 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 
 public class KeyWizard implements ClientModInitializer {
 	
 	public static final String MODID = "keywizard";
-	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
+	
+	private static KeyBinding keyOpenKeyWizard;
 
 	@Override
 	public void onInitializeClient() {
 		LOGGER.log(Level.DEBUG, MODID);
 		
+		keyOpenKeyWizard = KeyBindingHelper.registerKeyBinding(new KeyBinding("key." + MODID + ".openKeyWizard", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_F7, "category." + MODID + ".bindings"));
+		ClientTickEvents.register(client -> {
+			client.player.sendMessage(new LiteralText("Key 1 was pressed!"), false);
+		});
 	}
+	
+	
 	
 }
