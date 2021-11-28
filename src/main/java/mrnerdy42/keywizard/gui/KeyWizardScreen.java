f42b308e3ddd05be425bee77b927069a853a302a
Joseph Redmond <jdredmond2000@gmail.com>
2021-11-28 18:55:39 -0500

    The keyboard kinda works now

--- src/main/java/mrnerdy42/keywizard/gui/KeyWizardScreen.java
+++ src/main/java/mrnerdy42/keywizard/gui/KeyWizardScreen.java
@@ -1,7 +1,10 @@
 package mrnerdy42.keywizard.gui;
 
+import java.util.List;
+
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.option.GameOptions;
@@ -11,14 +14,18 @@ import net.minecraft.text.Text;
 
 @Environment(EnvType.CLIENT)
 public class KeyWizardScreen extends GameOptionsScreen{
+	
+	private KeyboardWidget keyboard;
 
 	public KeyWizardScreen(Screen parent, GameOptions gameOptions, Text title) {
 		super(parent, gameOptions, title);
+		this.keyboard = new KeyboardWidget(this.height/2, this.width/2, 100, 100);
+		this.addChild(keyboard);
 	}
 	
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
 		this.renderBackground(matrices);
-		this.drawHorizontalLine(matrices, this.width/2 - 20, this.width / 2 + 20, this.width/2, 0xFFFFFFFF);
+		this.keyboard.render(matrices, mouseX, mouseY, delta);
 		super.render(matrices, mouseX, mouseY, delta);
 	}
 
--- src/main/java/mrnerdy42/keywizard/gui/KeyboardWidget.java
+++ src/main/java/mrnerdy42/keywizard/gui/KeyboardWidget.java
@@ -1,49 +1,62 @@
 package mrnerdy42.keywizard.gui;
 
 import java.util.HashMap;
+import java.util.List;
 
 import mrnerdy42.keywizard.gui.KeyWizardScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.AbstractParentElement;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.PressableWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
-public class KeyboardWidget extends ClickableWidget{
-
-	public KeyboardWidget(int x, int y, int width, int height, Text message) {
-		super(x, y, width, height, message);
-		// TODO Auto-generated constructor stub
-	}
-
+public class KeyboardWidget extends AbstractParentElement{
 	public KeyWizardScreen parent;
 	
 	protected HashMap<Integer, KeyboardKeyWidget> keyList = new HashMap<>();
 	
 	private double scaleFactor;
+
+	public int x;
+	public int y;
 	
-	private class KeyboardKeyWidget extends PressableWidget{
+	public KeyboardWidget(int x, int y, int width, int height) {
+		this.keyList.put(0, new KeyboardKeyWidget(this.x, this.y, 20, 20, this));
+	}
+	
+	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		System.out.println("test");
+		for (KeyboardKeyWidget k : keyList.values()) {
+			k.render(matrices, mouseX, mouseY, delta);
+		}
+	}
+	
+	@Override
+	public List<? extends Element> children() {
+		return (List<? extends Element>) List.of(this.keyList.values());
+	}
+	
+	private class KeyboardKeyWidget extends PressableWidget implements Element{
 		
 		public int keyCode;
-		public String displayString;
+		//public String displayString;
 
-		protected boolean hovered;
+		//protected boolean hovered;
 		
 		private KeyboardWidget keyboard;
 		
 		public KeyboardKeyWidget(int x, int y, int width, int height, KeyboardWidget keyboard) {
-			super(x, y, width, height, Text.of(""));
+			super(x, y, width, height, Text.of("F"));
 			this.keyboard = keyboard;
+			this.visible = true;
 		}
 		
 		@Override
 		public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-			
-		}
-		
-
-		public void drawKey(Minecraft mc, double mouseX, double mouseY, float partialTicks) {
-			this.hovered = mouseX >= this.absX() && mouseY >= this.absY() && mouseX < this.absX() + this.width && mouseY < this.absY() + this.height;
+			this.hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
 			int modifiedBindings = 0;
 			int unmodifiedBindings = 0;
 			int color = 0;
@@ -66,11 +79,11 @@ public class KeyboardWidget extends ClickableWidget{
 			} else {
 				color = 0xFF555555;
 			}
-			
-			this.draw
-
-			drawNoFillRect(this.absX(), this.absY(), this.absX() + this.width, this.absY() + this.height, color);
-			drawCenteredString(this.keyboard.parent.getFontRenderer(), this.displayString, (float)(this.absX()+(this.width+2)/2.0F), (float)(this.absY()+(this.height-6)/2.0F), color & 0x00FFFFFF);
+			System.out.println("test");
+			drawNoFillRect(matrices, this.x, this.y, this.x + this.width, this.y + this.height, color);
+			//draw(matrices, getMessage(), (float)(this.x+(this.width+2)/2.0F), (float)(this.y+(this.height-6)/2.0F), color);
+			DrawableHelper.drawCenteredText(matrices, MinecraftClient.getInstance().textRenderer, "F", this.x/2, this.y/2, color);
+			//drawCenteredString(this.keyboard.parent.getFontRenderer(), this.displayString, (float)(this.absX()+(this.width+2)/2.0F), (float)(this.absY()+(this.height-6)/2.0F), color & 0x00FFFFFF);
 		}
 
 		@Override
@@ -80,18 +93,11 @@ public class KeyboardWidget extends ClickableWidget{
 			}
 		}
 		
-		protected void drawNoFillRect(double left, double top, double right, double bottom, int color) {
-			drawHorizontalLine(left, right, top, color);
-			drawHorizontalLine(left, right, bottom, color);
-			drawVerticalLine(left, top, bottom, color);
-			drawVerticalLine(right, top, bottom, color);
-		}
-		
-		public double absX() {
-			return this.keyboard.x + this.x;
-		}
-		public double absY() {
-			return this.keyboard.y + this.y;
+		protected void drawNoFillRect(MatrixStack matrices, int left, int top, int right, int bottom, int color) {
+			drawHorizontalLine(matrices, left, right, top, color);
+			drawHorizontalLine(matrices, left, right, bottom, color);
+			drawVerticalLine(matrices, left, top, bottom, color);
+			drawVerticalLine(matrices, right, top, bottom, color);
 		}
 	}
 
