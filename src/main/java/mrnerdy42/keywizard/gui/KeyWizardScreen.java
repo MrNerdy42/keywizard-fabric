f22e316004ed3c91162de06a31a07ee18a11a3e9
Joseph Redmond <jdredmond2000@gmail.com>
2022-01-05 18:31:45 -0500

    Dynamically sized list anyone?

--- src/main/java/mrnerdy42/keywizard/gui/KeyWizardScreen.java
+++ src/main/java/mrnerdy42/keywizard/gui/KeyWizardScreen.java
@@ -8,8 +8,10 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.LanguageOptionsScreen;
 import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 @Environment(EnvType.CLIENT)
 public class KeyWizardScreen extends GameOptionsScreen{
@@ -23,26 +25,28 @@ public class KeyWizardScreen extends GameOptionsScreen{
 	
 	@Override
 	protected void init() {
+		int maxBindingNameWidth = 0;
+		for (KeyBinding k : this.client.options.keysAll) {
+			int w = this.textRenderer.getWidth(new TranslatableText(k.getTranslationKey()));
+			if (w > maxBindingNameWidth)
+				maxBindingNameWidth = w;
+		}
+		
+		int bindingListWidth = (maxBindingNameWidth + 20);
 		this.keyboard = KeyboardWidgetBuilder.testKeyboard(this.width/2, this.height/2);
 		this.addChild(keyboard);
-		this.bindingList = new KeyBindingListWidget(client, 50, this.width/2, 200, 300, 50); 
+		//this.bindingList = new KeyBindingListWidget(client, 50, this.width/2, 200, 300, 50);
+		this.bindingList = new KeyBindingListWidget(this.client, 10, 10, bindingListWidth, this.height - 40, this.textRenderer.fontHeight * 3 + 10);
+		//this, 10, this.height - 30, bindingListWidth, this.height - 40, fontRenderer.FONT_HEIGHT * 3 + 10
 		this.addChild(this.bindingList);
-	}
-	
-	@Override
-	public boolean mouseClicked(double mouseX, double mouseY, int button) {
-		System.out.println("MouseX: %f MouseY: %f".formatted(mouseX, Math.abs(mouseY-this.height)));
-		return super.mouseClicked(mouseX, mouseY, button);
 	}
 	
 	@Override
 	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
 		this.renderBackground(matrices);
-		//this.keyboard.render(matrices, mouseX, mouseY, delta);
+		this.keyboard.render(matrices, mouseX, mouseY, delta);
 		this.bindingList.render(matrices, mouseX, mouseY, delta);
 		super.render(matrices, mouseX, mouseY, delta);
-		
-		//Gl coords start at bottom left and one unit is half of a minecraft unit
 	}
 
 }
