cd68d397d8f4d9693da72401baf2f8d1d02d741a
Joseph Redmond <jdredmond2000@gmail.com>
2022-04-28 16:37:52 -0400

    Mixin is ugly and I love it.

--- src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
+++ src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
@@ -2,6 +2,9 @@ package mrnerdy42.keywizard.mixin;
 
 import mrnerdy42.keywizard.KeyWizard;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+
+import java.lang.reflect.Field;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,9 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ControlsOptionsScreen.class)
 public class ControlsOptionsScreenInjector {
 	@Inject(at = @At("TAIL"), method = "init()V")
-	private void init(CallbackInfo info) {
+	private void init(CallbackInfo info) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		KeyWizard.LOGGER.info("Controls injector mixin loaded!");
 		ControlsOptionsScreen target = (ControlsOptionsScreen)((Object)this);
-
+		ButtonWidget resetButton = (ButtonWidget) (ControlsOptionsScreen.class.getDeclaredField("resetButton").get(target));
+		resetButton.setWidth(resetButton.getWidth() / 2);
 	}
 }
