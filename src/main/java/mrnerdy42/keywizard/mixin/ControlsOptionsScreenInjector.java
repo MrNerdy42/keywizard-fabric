075a1fb612a0900dc981011ec4ab07521f582765
Joseph Redmond <jdredmond2000@gmail.com>
2022-04-30 16:09:41 -0400

    Reflection sucks.

--- src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
+++ src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
@@ -1,10 +1,16 @@
 package mrnerdy42.keywizard.mixin;
 
 import mrnerdy42.keywizard.KeyWizard;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.TranslatableText;
 
 import java.lang.reflect.Field;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,10 +20,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ControlsOptionsScreen.class)
 public class ControlsOptionsScreenInjector {
 	@Inject(at = @At("TAIL"), method = "init()V")
-	private void init(CallbackInfo info) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+	private void init(CallbackInfo info) {
 		KeyWizard.LOGGER.info("Controls injector mixin loaded!");
 		ControlsOptionsScreen target = (ControlsOptionsScreen)((Object)this);
-		ButtonWidget resetButton = (ButtonWidget) (ControlsOptionsScreen.class.getDeclaredField("resetButton").get(target));
-		resetButton.setWidth(resetButton.getWidth() / 2);
+		
 	}
 }
