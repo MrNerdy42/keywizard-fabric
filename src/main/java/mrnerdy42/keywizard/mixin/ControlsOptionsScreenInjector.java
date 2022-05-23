a0aa6e4f181a9a06f143f442723f7f81e4edde6a
Joseph Redmond <jdredmond2000@gmail.com>
2022-05-23 11:10:29 -0400

    Useless constructors are useless.

--- src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
+++ src/main/java/mrnerdy42/keywizard/mixin/ControlsOptionsScreenInjector.java
@@ -1,51 +1,29 @@
 package mrnerdy42.keywizard.mixin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Field;
-
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import mrnerdy42.keywizard.KeyWizard;
 import mrnerdy42.keywizard.gui.KeyWizardScreen;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.ControlsOptionsScreen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.text.Text;
 
 @Mixin(ControlsOptionsScreen.class)
 public abstract class ControlsOptionsScreenInjector extends GameOptionsScreen {
 	
-	private ControlsOptionsScreenInjector(Screen parent, GameOptions gameOptions, Text title) {
-		super(parent, gameOptions, title);
+	// Useless constructor to make the compiler happy. Don't ever call it.
+	private ControlsOptionsScreenInjector() {
+		super(null, null, null);
 	}
-
-	@Shadow
-	protected abstract <T extends ClickableWidget> T addButton(T button);
-	@Shadow
-	@Final
-	protected Screen parent;
-	@Shadow
-	protected MinecraftClient client;
-	
-	@Shadow
-	public int width;
 
 	@Inject(at = @At("TAIL"), method = "init()V")
 	private void init(CallbackInfo info) {
 		KeyWizard.LOGGER.info("Controls screen injector mixin loaded!");
-		ControlsOptionsScreen target = (ControlsOptionsScreen)((Object)this);
-		TexturedButtonWidget screenToggleButton = new TexturedButtonWidget(this.width - 22, target.height - 22, 20, 20, 0, 0, 20, KeyWizard.SCREEN_TOGGLE_WIDGETS, 40, 40, (btn) -> {
-		    client.openScreen(new KeyWizardScreen(parent));
+		TexturedButtonWidget screenToggleButton = new TexturedButtonWidget(this.width - 22, this.height - 22, 20, 20, 0, 0, 20, KeyWizard.SCREEN_TOGGLE_WIDGETS, 40, 40, (btn) -> {
+		    client.openScreen(new KeyWizardScreen(this.parent));
 		});
 		this.addButton(screenToggleButton);
 	}
