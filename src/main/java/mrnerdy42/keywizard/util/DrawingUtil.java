f003394e9725cadf3f578f27260950c1c2730a40
Joseph Redmond <jdredmond2000@gmail.com>
2022-11-28 09:07:02 -0500

    Fix key rendering.

--- src/main/java/mrnerdy42/keywizard/util/DrawingUtil.java
+++ src/main/java/mrnerdy42/keywizard/util/DrawingUtil.java
@@ -41,7 +41,7 @@ public class DrawingUtil {
 		bufferBuilder.vertex(matrix, x2, y2, 0.0F).color(g, h, k, f).next();
 		bufferBuilder.vertex(matrix, x2, y1, 0.0F).color(g, h, k, f).next();
 		bufferBuilder.vertex(matrix, x1, y1, 0.0F).color(g, h, k, f).next();
-		BufferRenderer.drawWithoutShader(bufferBuilder.end());
+		BufferRenderer.drawWithShader(bufferBuilder.end());
 		RenderSystem.enableTexture();
 		RenderSystem.disableBlend();
 	}
